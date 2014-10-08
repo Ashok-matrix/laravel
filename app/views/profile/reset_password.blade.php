be28d5097fe9d713f9e5575ede7bc6b060bfb4e7
Ashok-matrix <ashok.matrixinfologics@gmail.com>
2014-10-08 18:39:45 +0530

    Update reset_password.blade.php

--- app/views/profile/reset_password.blade.php
+++ app/views/profile/reset_password.blade.php
@@ -1,22 +1,17 @@
-{{ Form::open() }}
-	<div class="form-group">
-		{{ Form::label('old_password', 'Old Password:') }}
-		{{ Form::password('old_password', null, ['class' => 'form-control']) }}
-	</div>
+@extends('layouts.default')
 
-	<div class="form-group">
-		{{ Form::label('new_password', 'New Password:') }}
-		{{ Form::password('new_password', null, ['class' => 'form-control']) }}
+@section('content')
+	<div class="row">
+		<div class="col-md-6">
+			<h1>Reset Password</h1>
+			@include('layouts.partials.errors')
+			 @if (Session::has('error'))
+				<div class="alert alert-danger"><ul>
+				</li>{{ Session::get('error') }}</li>
+				</ul></div>
+		   
+		   	 @endif
+			@include('layouts.partials.forms.reset_password')
+		</div>
 	</div>
-
-	<div class="form-group">
-		{{ Form::label('password_confirmation', 'Confirm Password:') }}
-		{{ Form::password('password_confirmation', null ,['class' => 'form-control']) }}
-	</div>
-
-	
-	<div class="form-group">
-		{{ Form::submit('Reset Password', ['id' => 'reset-password','class' => 'btn btn-primary']) }}
-	</div>
-
-{{ Form::close() }}
+@stop
